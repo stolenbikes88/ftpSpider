40aeb73a2548b75e21bcb8f2ef1ae8561ede7757
root <root@localhost.localdomain>
2016-05-12 18:23:19 +0000

    finder now working, extract needs work.

--- extractor/ftpextractor.py
+++ extractor/ftpextractor.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python
-import sys, os, argparse, urllib2
+import sys, os, argparse, urllib2,json
 from urllib2 import Request
 #from urllib.request import urlopen
 from multiprocessing import Pool
+from pprint import pprint
 
-def findftp(domain):
-	domain = domain.strip()
+def extractftp(line):
+	linesplit = line.split(",")
+	domain = linesplit[0].strip()
     	
     	try:
 		# TAKE A LOOK FOR FTP Configuration file
 		# Try to download http://target.tld/sftp-config.json
 		request = Request('http://' + domain + "/sftp-config.json")
 		req = urllib2.urlopen(request)
-		answer = req.read(250).decode()
-		print answer
+		answer = req.read().decode()
 
 		# Check if refs/heads is in the file
 		if('save_before_upload' in answer):
+                    data = json.loads(answer)
 		    # Write match to OUTPUTFILE
-		    fHandle = open(OUTPUTFILE,'a')
-		    fHandle.write(domain + ", sftp-config.json, "+req.headers.get('content-length')+"\n")
-		    fHandle.close()
-		    print("[*] Found config: " + domain)
+		    #fHandle = open(OUTPUTFILE,'a')
+		    #fHandle.write(domain + ", sftp-config.json, "+req.headers.get('content-length')+"\n")
+		    #fHandle.close()
+		    #print("[*] Found config: " + domain)
 		    return
 
 	except Exception as e:     
+		print e
         	print("[*] Nope: " + domain)
     
 
@@ -32,17 +35,18 @@ if __name__ == '__main__':
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--inputfile', default='domains.txt', help='input file')
+    parser.add_argument('-i', '--inputfile', default='extract.txt', help='input file')
     parser.add_argument('-o', '--outputfile', default='output.txt', help='output file')
     parser.add_argument('-t', '--threads', default=200, help='threads')
     args = parser.parse_args()
 
-    DOMAINFILE=args.inputfile
+    EXTRACTFILE=args.inputfile
     OUTPUTFILE=args.outputfile
     MAXPROCESSES=int(args.threads)
 
     print("Scanning...")
     pool = Pool(processes=MAXPROCESSES)
-    domains = open(DOMAINFILE, "r").readlines()
-    pool.map(findftp, domains)
-    print("Finished")
+    extracts = open(EXTRACTFILE, "r").readlines()
+
+    pool.map(extractftp, extracts)
+    #print("Finished")
